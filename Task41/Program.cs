72d192e1ae8b3710c1a487d9517ea13b8fca96b8
UrsAlex <ursalex@mail.ru>
2023-01-06 07:24:14 +0300

    Task 41, 43

---
+++ Task41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисео больше 0 ввел пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223 -> 3
+
+
+// int PrintArray(string text)
+// {
+//     Console.WriteLine(text);
+//     int num = int.Parse(Console.ReadLine());
+//     return num;
+// }
+
+// PrintArray("введите числа");
+
+
+Console.Write("ВВедите числа (через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+Console.WriteLine($"Количество чисел больше 0 равно {count}");
---
+++ Task43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая найдет точку пересечения двух прямых, заданных уравнениями 
+// у = k1 * x + b1, у = k2 * x + b2; значения b1, k1, b2, k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void IntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    double x = (b1 - b2) / (k2 - k1);
+    double y = (k1 * x) + b1;
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
+}
+
+double EnterNum(string text)
+{
+    Console.WriteLine(text);
+    double num = double.Parse(Console.ReadLine());
+    return num;
+}
+
+double b1 = EnterNum("Введите точку b1: ");
+double k1 = EnterNum("Введите точку k1: ");
+double b2 = EnterNum("Введите точку b2: ");
+double k2 = EnterNum("Введите точку k2: ");
+IntersectionPoint(b1, k1, b2, k2);
